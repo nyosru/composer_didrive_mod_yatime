ca630148f0213095c755b72e213e42833c34a633
nyosru <nyos@rambler.ru>
2020-02-26 17:29:28 +0500

    0.0.3

--- class.php
+++ class.php
@@ -9,14 +9,14 @@ namespace Nyos\mod;
 if (!defined('IN_NYOS_PROJECT'))
     throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
 
-class mod_class {
-
-    public static $dir_img_server = false;
-
-    public static function creatFolderImage($folder) {
-        /**
-        
-        **/
-    }
-
-}
+//class mod_class {
+//
+//    public static $dir_img_server = false;
+//
+//    public static function creatFolderImage($folder) {
+//        /**
+//        
+//        **/
+//    }
+//
+//}
